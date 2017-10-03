befcb601571a53db525706c3ac668e67e3adef0d
kirstenmyette <kirstenmyette@aol.com>
2017-10-03 16:18:46 -0500

    .

--- script.js
+++ script.js
@@ -1,7 +1,43 @@
-$(document).ready(function () { 
+// var trueKeyCode = configureTrueKeyCode(keyCode, isShift);
 
-$(document).bind('keydown', (function (e) {
-    if('#kyboard-upper-container' + e.keyCode === 16) {
-       $('#keyboard-upper-container').css('visibility', 'visible');}
+// function configureTrueKeyCode(keyCode, isShift) {
+//     if (isShift === false && keyCode >= 65 && keyCode <= 90) {
+//         return keyCode + 32;
+//     }
+
+//     return keyCode;
+// }
+
+$(document).ready(function () {
+$('#keyboard-upper-container').hide();
+
+
+  $(document).keydown(function (event) {
+    $("#" + event.which).css('background', 'yellow');
+    if(event.which !==16) {
+       return event.which + 32;
+    }
+    if (event.which == 16){ 
+        $('#keyboard-upper-container').show();
+        $('#keyboard-lower-container').hide();
+    }
     });
-});
+
+
+
+    $(document).keyup(function (event) {
+      $("#" + event.which).css('background', '#f5f5f5');
+      if (event.which == 16){ 
+            $('#keyboard-upper-container').hide();
+            $('#keyboard-lower-container').show();
+        }
+    });
+
+
+    //end of doc ready function aka leave this one alone!
+    });
+
+
+
+
+
