b8995998f2a68e2803ee14ef8169c232a093c4bf
kirstenmyette <kirstenmyette@aol.com>
2017-10-04 16:36:56 -0500

    starting over using first code you gave us

--- script.js
+++ script.js
@@ -1,43 +1,89 @@
-// var trueKeyCode = configureTrueKeyCode(keyCode, isShift);
+var sentences = [
+    'this is my first sentence',
+    'Too ato too nOt enot one totA not anot tOO aNot',
+    'oat itain oat tainnate eate tea anne inant nean',
+    'itant eate anot eat nato inate eatanot tain eat',
+    'nee ene ate ite tent tiet ent ine ene ete ene ate'
+];
+var sentenceCounter = sentences[0];
+var currentSentence = sentences[sentenceCounter];
+var sentenceContainer
 
-// function configureTrueKeyCode(keyCode, isShift) {
-//     if (isShift === false && keyCode >= 65 && keyCode <= 90) {
-//         return keyCode + 32;
-//     }
-
-//     return keyCode;
-// }
-
-$(document).ready(function () {
-$('#keyboard-upper-container').hide();
+var characterCounter = 0;
+var upper,
+    lower;
+    
 
 
-  $(document).keydown(function (event) {
-    $("#" + event.which).css('background', 'yellow');
-    if(event.which !==16) {
-       return event.which + 32;
-    }
-    if (event.which == 16){ 
-        $('#keyboard-upper-container').show();
-        $('#keyboard-lower-container').hide();
-    }
+
+$(document).ready(() => {
+    upper = $('#keyboard-upper-container');
+    lower = $('#keyboard-lower-container');
+    sentenceContainer = $(sentences);
+
+ toggleKeyboards();
+ appendSentences();
+ countSentence();
+
+    $(document).on('keyup keydown', (e) => {
+        handleKeyPress(e.type, e.shiftKey, e.keyCode);
     });
+});
 
-
-
-    $(document).keyup(function (event) {
-      $("#" + event.which).css('background', '#f5f5f5');
-      if (event.which == 16){ 
-            $('#keyboard-upper-container').hide();
-            $('#keyboard-lower-container').show();
+function handleKeyPress(type, isShift, keyCode) {
+    if (keyCode === 16) {
+        if (type === 'keydown') {
+            toggleKeyboards(true);
+        } else {
+            toggleKeyboards();
         }
-    });
+    }
+
+    var trueKeyCode = configureTrueKeyCode(keyCode, isShift);
+
+    handleHighlight(trueKeyCode, type);
+}
+
+function handleHighlight(keyCode, type) {
+    if (type === 'keydown') {
+        $('#' + keyCode).css('background-color', 'yellow');
+    } else {
+        $('#' + keyCode).css('background-color', '#f5f5f5');
+    }
+}
+
+function configureTrueKeyCode(keyCode, isShift) {
+    if (isShift === false && keyCode >= 65 && keyCode <= 90) {
+        return keyCode + 32;
+    }
+
+    return keyCode;
+}
+
+function toggleKeyboards(showUpper) {
+    if (showUpper) {
+        lower.hide();
+        upper.show();
+    } else {
+        upper.hide();
+        lower.show();
+    }
+}
+
+function appendSentences() { 
+    $('#prompt-container').append(sentences[0]);
+    
+}
+
+function countSentence () {
+   
+}
 
 
-    //end of doc ready function aka leave this one alone!
-    });
-
-
-
-
-
+// function appendSentences () {
+//     var currentSentence = sentences[sentenceCounter],
+//     splitSentence = currentSentence.split('');
+//     splitSentence.forEach((s) => {
+//         $('<span id="letter-' + i + '">' + s  + '</span>').appendTo(sentenceContainer);
+//     });
+// }
